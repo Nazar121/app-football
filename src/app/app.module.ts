39f6e8a81936797941896e9ebcd52d9412c65998
Nazar Burban <BurbaNazar98@gmail.com>
2020-11-26 18:05:32 +0200

    Created Core and Shared modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Modules
+import { CoreModule } from '@core/core.module';
+
+// App
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,6 +14,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    CoreModule,
     AppRoutingModule
   ],
   providers: [],
